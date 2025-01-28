e0ee9a3dfceae7f91e51a377a318f58aa93c0e26
Pedro Minor <pemi3637@colorado.edu>
2025-01-27 17:26:49 -0700

    added configurations to sphinx

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -11,6 +11,7 @@
 import os
 import sys
 sys.path.insert(0, os.path.abspath('../../'))
+autodoc_member_order = 'bysource'
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
@@ -31,6 +32,8 @@ extensions = [
 
 templates_path = ['_templates']
 exclude_patterns = []
+
+autodoc_mock_imports = ['HARPY']
 
 
 
